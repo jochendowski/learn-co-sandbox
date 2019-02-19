d041eba9e7315273831ec205ae65a01ee81a6eb7
Justin H Ochendowski <jochendowski@gmail.com>
2019-02-19 06:33:56 +0000

    Automatically backed up by Learn

--- functions.js
+++ functions.js
@@ -1,4 +1,5 @@
 function greeting(name, greeting, byeMessage) {
   console.log("I will print first");
   return `${greeting} there, ${name}! It was great seeing you, ${byeMessage}!`;
+  console.log("I don't print");
 }
