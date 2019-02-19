341a8123bb3db0f32c4dbbdfbb4f973ca16f1f74
Justin H Ochendowski <jochendowski@gmail.com>
2019-02-19 06:35:08 +0000

    Automatically backed up by Learn

--- functions.js
+++ functions.js
@@ -4,4 +4,4 @@ function greeting(name, greeting, byeMessage) {
   console.log("I don't print");
 }
 
-greeting("Cho", "Yo-Yo", "See ya")
+greeting("Cho", "Yo-Yo", "See ya") // Yo-Yo there, Cho! It was great seeint you, See ya!
