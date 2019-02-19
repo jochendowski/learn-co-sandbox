f6180362f87d8ee28ce1224f7e65698a9d5ebe17
Justin H Ochendowski <jochendowski@gmail.com>
2019-02-19 06:09:24 +0000

    Automatically backed up by Learn

---
+++ variables.js
@@ -0,0 +1,15 @@
+// declare variable and log
+
+var greeting = "Hello, how are you?!";
+
+console.log(greeting);
+
+// create new variable using con
+var return = "I am well!" + " " + greeting;
+
+console.log(return);
+
+
+// use string interpolation 
+    
+    
