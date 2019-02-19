0e5c7e925f7cf6c10a59a3470e9dae93eb6e27c7
Justin H Ochendowski <jochendowski@gmail.com>
2019-02-19 06:31:50 +0000

    Automatically backed up by Learn

--- functions.js
+++ functions.js
@@ -1,12 +1,1 @@
-function sayHi(name) {
-  console.log("Hello " + name + "!");
-}
-
-sayHi("Cho");
-
-function sayHiBack(name, greeting) {
-  console.log("You said hello first");
-  return `${greeting}, ${name} it's been awhile!`;
-}
-
-sayHiBack("Ba", "Yo!")
+function 
